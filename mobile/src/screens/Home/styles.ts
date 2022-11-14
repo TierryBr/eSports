d3e31c4059a75346baeb20077c49991a44c1eefb
Tierry Barros <franciso.oliveira@dellead.com>
2022-11-14 15:13:13 -0300

    Create Heading componente

--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,5 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  logo: {
+    width: 234,
+    height: 135,
+    marginTop: 75,
+    marginBottom: 48
+  }
 });
